eb4516b0ec1d9aae6798fd5dd7d207bdb750668f
Morfusee <markvalenzuela.coding@gmail.com>
2025-06-22 14:40:32 +0800

    update: moved generation of reports to reportController

--- frontend/src/controllers/payrollController.ts
+++ frontend/src/controllers/payrollController.ts
@@ -1,10 +1,6 @@
 import { notifications } from "@mantine/notifications";
 import { reportRequestNotification } from "../constants/notificationData";
-import {
-  payrollApi,
-  payrollGenerateReport,
-  payrollMonthsApi,
-} from "../services/payrollService";
+import { payrollApi, payrollMonthsApi } from "../services/payrollService";
 import {
   getFilenameFromContentDisposition,
   downloadFile,
@@ -43,64 +39,7 @@ function payrollController() {
     }
   };
 
-  const getGeneratePayrollReport = async () => {
-    const notificationId = `generate-report-notification-${Date.now()}`; // More robust ID
-
-    // Show the initial loading notification
-    notifications.show({
-      loading: true,
-      withBorder: true,
-      autoClose: false,
-      withCloseButton: false,
-      id: notificationId,
-      title: reportRequestNotification.generate.loading.title,
-      message: reportRequestNotification.generate.loading.message,
-    });
-
-    try {
-      const response = await payrollGenerateReport();
-
-      const blob = response.data;
-
-      // Create a temporary download URL
-      const url = window.URL.createObjectURL(blob);
-
-      // Safely extract the filename
-      const filename = getFilenameFromContentDisposition(
-        "report.pdf",
-        response.headers?.["content-disposition"] as string
-      );
-
-      // Trigger the file download
-      downloadFile(url, filename);
-
-      // Clean up the object URL
-      window.URL.revokeObjectURL(url);
-
-      // Show notification
-      notifications.update({
-        id: notificationId,
-        withBorder: true,
-        autoClose: 5000,
-        loading: false,
-        title: reportRequestNotification.generate.success.title,
-        message: reportRequestNotification.generate.success.message,
-      });
-    } catch (error) {
-      // Show notification
-      notifications.show({
-        withBorder: true,
-        autoClose: 5000,
-        title: reportRequestNotification.generate.error.title,
-        message: reportRequestNotification.generate.error.message,
-        color: "red",
-      });
-
-      console.error("Error generating report:", error);
-    }
-  };
-
-  return { getPayroll, getPayrollMonths, getGeneratePayrollReport };
+  return { getPayroll, getPayrollMonths };
 }
 
 export default payrollController;
